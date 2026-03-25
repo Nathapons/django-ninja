8ac442699a2c49cc86c2de508e3f7d02327bbf3f
Nuthapon <nuthaponm79@gmail.com>
2026-03-25 21:52:11 +0700

    feat: install django extensions

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,15 +30,27 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_SETTINGS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+]
+
+THIRD_PARTY_APPS = [
+    'django_extensions',
+]
+
+DJANGO_APPS = [
     'apps.users',
 ]
+
+INSTALLED_APPS = DJANGO_SETTINGS + THIRD_PARTY_APPS + DJANGO_APPS
+
+SHELL_PLUS = 'ipython'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
