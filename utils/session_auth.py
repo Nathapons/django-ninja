1145a020c36d6c5f4a4307867505bf1c3a6ff6ca
Nuthapon <nuthaponm79@gmail.com>
2026-04-02 23:50:37 +0700

    feat: custom auth

--- utils/session_auth.py
+++ utils/session_auth.py
@@ -1,3 +1,6 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+
 from ninja.security.session import SessionAuth
 
 
@@ -6,5 +9,5 @@ class CustomSessionAuth(SessionAuth):
     def authenticate(self, request, key):
         user = super().authenticate(request, key)
         if user is None:
-            return None
+            return redirect(reverse("api-1.0.0:user_unauthorized_page"))
         return user
