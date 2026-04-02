0c294f53878bd3ca404a086af86bef8687e16a45
Nuthapon <nuthaponm79@gmail.com>
2026-04-03 00:14:01 +0700

    feat: install postgres sql

--- myproject/settings.py
+++ myproject/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'precision_plastic_lab'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgrespassword'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
