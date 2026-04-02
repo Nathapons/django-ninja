4fc2c2ad8b689d13efe31b0678dddcc5c797ea1f
Nuthapon <nuthaponm79@gmail.com>
2026-04-03 00:03:45 +0700

    feat: add menu_base.html

--- myproject/settings.py
+++ myproject/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
